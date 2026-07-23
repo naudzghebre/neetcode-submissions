990509f95916cbe28762f0ba7c983d6c648fce60
Naud Ghebre <30516046+naudzghebre@users.noreply.github.com>
2026-07-23 16:00:38 -0400

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+
+    # def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+    # Construct list and then run binary search
+    # O(m*n + log m*n) = O(m*n)
+    # def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    #     # O(m*n)
+    #     mn = (len(matrix) * len(matrix[0]))
+    #     matrixList = mn *[0]
+    #     i = 0
+    #     for r in matrix:
+    #         for c in r:
+    #             matrixList[i] = c
+    #             i += 1
+
+    #     # O(log m*n)
+    #     l, r = 0, mn - 1
+    #     while l <= r:
+    #         m = (l + r) // 2
+    #         if matrixList[m] == target: return True
+    #         elif matrixList[m] < target: l = m + 1
+    #         else: r = m - 1
+    #     return False
+
+    # Linear pass brute force
+    # O(m*n)
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for r in matrix:
+            for c in r:
+                if c == target: return True
+        return False
+
+
+        
