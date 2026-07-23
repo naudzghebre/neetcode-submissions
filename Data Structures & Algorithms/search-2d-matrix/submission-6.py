6381b36fe1c2316d0cd8b5b68b15e3297957b8f9
Naud Ghebre <30516046+naudzghebre@users.noreply.github.com>
2026-07-23 16:22:55 -0400

    Add: search-2d-matrix - submission-6

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-6.py
@@ -0,0 +1,66 @@
+class Solution:
+
+    # 4. Staircase smart search going down the rows and checking last value till
+    # you're sure about which row it's in - linear search
+    # O(m + log n) 
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for r in matrix:
+            if r[-1] < target: continue # Go down
+
+            for c in r:
+                if c == target: return True
+            return False
+        return False
+
+    # 4. Staircase smart search going down the rows and checking last value till
+    # you're sure about which row it's in - run binary search
+    # O(m + log n) 
+    # def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    #     for r in matrix:
+    #         if r[-1] < target: continue # Go down
+    #         else:
+    #             return self.binarySearch(r, target)
+    #     return False
+
+
+    # 3. Binary search over every row
+    # O(m log n)
+    # def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    #     for r in matrix:
+    #         if self.binarySearch(r, target): return True
+    #     return False
+
+    # 2. Construct list and then run binary search
+    # O(m*n + log m*n) = O(m*n)
+    # def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    #     # O(m*n)
+    #     mn = (len(matrix) * len(matrix[0]))
+    #     matrixList = mn *[0]
+    #     i = 0
+    #     for r in matrix:
+    #         for c in r:
+    #             matrixList[i] = c
+    #             i += 1
+    #     return self.binarySearch(matrixList, target) # Passing in combined list so - O(log m*n)
+
+    # 1. Linear pass brute force
+    # O(m*n)
+    # def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    #     for r in matrix:
+    #         for c in r:
+    #             if c == target: return True
+    #     return False
+
+    # Helper binary search method
+    # O(log len(nums))
+    def binarySearch(self, nums: list, target: int) -> bool:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target: return True
+            elif nums[m] < target: l = m + 1
+            else: r = m - 1
+        return False
+
+
+        
