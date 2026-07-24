d09c9d7d95a3da9023b706fe7ac7c5e6effd26fb
Naud Ghebre <30516046+naudzghebre@users.noreply.github.com>
2026-07-24 16:28:53 -0400

    Add: delete-node-in-a-bst - submission-0

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-0.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findMin(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        curr = root
+        while True:
+            if curr and curr.left: curr = curr.left
+            else: return curr
+
+    def findMax(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        curr = root
+        while True:
+            if curr.right: curr = curr.right
+            else: return curr
+            
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left: return root.right
+            elif not root.right: return root.left
+            else:
+                minRight = self.findMin(root.right)
+                root.val = minRight.val
+                root.right = self.deleteNode(root.right, root.val)
+        return root
+                
