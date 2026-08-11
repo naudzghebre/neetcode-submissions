c199a470272af7c992857bc7efe2674a1fd15e4d
Naud Ghebre <30516046+naudzghebre@users.noreply.github.com>
2026-08-11 17:42:46 -0400

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+
+    # Even nicer trick
+    def countBits(self, n: int) -> List[int]:
+        arr = [0,1]
+        while len(arr) <= n:
+            temp_arr = [x + 1 for x in arr]
+            arr = arr + temp_arr
+        return arr[0:n+1]
+
+    # Nice shorthand for the below solution
+    # def countBits(self, n: int) -> List[int]:
+    #     counts = [0] * (n + 1)
+    #     for i in range(1, n + 1):
+    #         counts[i] = counts[i >> 1] + (i & 1)
+    #     return counts    
+
+    # If it's even, has the same num of 1s as num // 2 (floor)
+    # else, has same num as num // 2 plus the odd bit
+    # def countBits(self, n: int) -> List[int]:
+    #     counts = [0] * (n+1)
+    #     for i in range(1, n+1):
+    #         if i % 2 == 0: counts[i] = counts[i // 2]
+    #         else: counts[i] = counts[i // 2] + 1
+    #     return counts
+
+    # O(n log n) - we can do better
+    # def countBits(self, n: int) -> List[int]:
+    #     counts = [0] * (n+1)
+    #     for i in range(1, n+1):
+    #         bits, count = i, 0
+    #         while bits > 0:
+    #             if bits & 1 == 1:
+    #                 count += 1
+    #             bits //= 2
+    #         counts[i] = count
+    #     return counts
